6ea0481aa4fd5f1669f63868dcca384687608a0c
Kirito <2493381254@qq.com>
2024-01-13 02:13:33 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/header.jpg
 
 
-// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/loader/loading.jpg
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/loading.jpg
 
-// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/bg/cover.jpg
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/cover.jpg
