bdd14009946bcf6e4a5c7af1081cf5c0e445deb9
Kirito <2493381254@qq.com>
2024-01-13 02:11:13 +0800

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/header.jpg
+
+
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/loader/loading.jpg
+
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/bg/cover.jpg
