7cf1e4ec17a64a023dd47d91356f275c62739021
Kirito <2493381254@qq.com>
2024-03-28 17:05:10 +0800

    update

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@
 
 // https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/loading.jpg
 
+
 // https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/cover.jpg
+
+
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/header.jpg
+
+// https://cdn.jsdelivr.net/gh/Kiritoabc/my_cdn/img/菠萝.png
